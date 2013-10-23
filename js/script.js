43c3463fa8bfc835d2799fefc4eee31392cfcf5a
Mohammad Reza Shariatzadeh <mrshcom@yahoo.com>
2013-10-23 16:31:02 +0330

    plau audio

--- js/script.js
+++ js/script.js
@@ -6,6 +6,21 @@ function onDeviceReady() {
   setTimeout(function(){
     $.ui.launch();
   },50);
+
+  // AppMobi.player.volume(90);
+
+  // AppMobi.player.loadSound("../sounds/01.mp3",5);
+  AppMobi.player.loadSound("sounds/01.mp3");
+  function loadSoundError()
+  {
+    alert("فایل صوتی یافت نشد!");
+  }
+  document.addEventListener("appMobi.player.sound.error",loadSoundError,false);
+
+  AppMobi.player.play();
+
+  alert('Audio Current Track Length: ' + AppMobi.player.audioInfo.duration);
+
 }
 
 document.addEventListener("appMobi.device.ready",onDeviceReady,false);
@@ -17,3 +32,4 @@ $(document).ready(function(){
   //if($.ui.useOSThemes&&!$.os.ios&&$("#afui").get(0).className!=="ios")
   //$("#afui").removeClass("ios");
 });
+
